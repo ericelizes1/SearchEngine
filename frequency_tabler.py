4ef7066a9c61fa49afe666ddfe862cd2356e2611
Jake Sanghavi <57878447+jakesanghavi@users.noreply.github.com>
2021-11-30 13:29:25 -0500

    Update frequency_tabler.py

--- frequency_tabler.py
+++ frequency_tabler.py
@@ -7,6 +7,8 @@ data = pd.read_csv("data.csv", delimiter="~")
 
 new_data = pd.DataFrame(columns=['Title', 'Link', 'Dict'])
 
+# Use regex to split the website description up by word and make a dictionary from it. If the word appears
+# multiple times, increase its value by 1 for each repeat.
 for i in range(0, len(data)):
     holder = dict(Counter(list(filter(None, re.split(r"[ 1234567890,.!?|<>~`@#$%^&*()_+={}:;\\\"\'\[\]/]", str(data.Desc.iloc[i]).lower())))))
     new_data.loc[len(new_data)] = [data.Title.iloc[i], data.Link.iloc[i], holder]
