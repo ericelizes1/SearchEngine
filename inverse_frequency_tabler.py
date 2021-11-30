2b3650bf965cdc448447e22024f0ad3105bad08e
Jake Sanghavi <57878447+jakesanghavi@users.noreply.github.com>
2021-11-30 13:28:11 -0500

    Update inverse_frequency_tabler.py

--- inverse_frequency_tabler.py
+++ inverse_frequency_tabler.py
@@ -1,21 +1,25 @@
 import pandas as pd
 from ast import literal_eval
 import numpy as np
-from datetime import datetime
+
+'''
+To Do:
+1. Use boolean array to apply operation once instead of for loop?
+2. Instead of above, use np.vectorize to remove loops? (Hard to do with nested loops)
+3. Use map() instead?
+4. Is it faster to make a list of tuples and map these tuples to a dictionary afterwards?
+'''
 
 data = pd.read_csv("new_data.csv", delimiter="~")
-
 data.columns = ['title', 'links', 'dict']
-
 data = data.dropna()
 
-list_d = list(data.index.values)
-list_d = list_d[:-1]
-
+# Make column a dictionary again (pandas does not recognize dictionaries as a type when reading)
 data.dict = data.dict.apply(literal_eval)
 
 unique_words = []
 
+# Add all unique words to an array to be used as a column for the output dataset.
 for x in range(0, len(data)):
     for y in range(0, len(data['dict'].iloc[x])):
         if list(data['dict'].iloc[x].keys())[y] not in unique_words:
@@ -23,6 +27,8 @@ for x in range(0, len(data)):
 
 output = pd.DataFrame(columns=['word', 'count'])
 
+# For each unique word, search for it in the dictionary of words for each website, and create a dictionary of websites and
+# the frequency with which the word appears in each one, if at all.
 for x in range(0, len(unique_words)):
     hol_dict = {}
     for y in range(0, len(data)):
