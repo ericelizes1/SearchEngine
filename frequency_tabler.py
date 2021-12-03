fd7683a9344946bc93bdb128ec8722a21d8dc0d1
Jake Sanghavi <57878447+jakesanghavi@users.noreply.github.com>
2021-12-02 21:50:21 -0500

    Update frequency_tabler.py

--- frequency_tabler.py
+++ frequency_tabler.py
@@ -3,6 +3,7 @@ from collections import Counter
 import re
 import numpy as np
 
+# Read the data file produced by spider.py
 data = pd.read_csv("data.csv", delimiter="~")
 
 new_data = pd.DataFrame(columns=['Title', 'Link', 'Dict'])
@@ -13,4 +14,5 @@ for i in range(0, len(data)):
     holder = dict(Counter(list(filter(None, re.split(r"[ 1234567890,.!?|<>~`@#$%^&*()_+={}:;\\\"\'\[\]/]", str(data.Desc.iloc[i]).lower())))))
     new_data.loc[len(new_data)] = [data.Title.iloc[i], data.Link.iloc[i], holder]
 
+# Save this data to csv
 np.savetxt("new_data.csv", new_data, delimiter="~", fmt="%s", encoding="utf-8")
