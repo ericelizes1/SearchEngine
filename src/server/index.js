db09b2617e0dae423117c0c28047e8c2cd53ee2c
Gaynell Jainschigg <91689742+GaynellJainschigg@users.noreply.github.com>
2021-12-09 14:12:56 -0500

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -14,9 +14,10 @@ kw = req.query.keyword
 
 // read files
 
-data = fs.readFileSync("data.csv",{encoding:'utf8', flag:'r'})
-inv = fs.readFileSync("inv_freq.csv",{encoding:'utf8', flag:'r'})
- 
+data = fs.readFileSync("spiderdata.csv",{encoding:'utf8', flag:'r'})
+inv = fs.readFileSync("shortened.csv",{encoding:'utf8', flag:'r'})
+
+console.log('1')
 // clean data.csv
 
 data = data.split('\r\n')
@@ -33,11 +34,12 @@ for(i = 0; i < newdata.length; i++){
 
 data = newdata
 
-// clean inv_freq.csv
+console.log('2')
+// clean shortened.csv
 
 inv = inv.split('\r\n')
 newinv = []
-for(i = 0;i < inv.length;i++){
+for(i = 0; i < inv.length; i++){
     line = inv[i].split('~')
  
 if (line[1] != undefined){
@@ -52,41 +54,45 @@ newinv.push(line)
 inv = newinv
  
 // build result data structure
-
+console.log('3')
 // lowercase the input keyword
 
 kw = kw.trim()
 kw = kw.toLowerCase()
 
-// find the word in the inv_freq array and get its list of links and frequencies
-
+// find the word in the inv_freq array 
+// stores its list of links and frequencies
 var msg = "Oh no! We can't find that word!"
 
-for(i = 0;i < inv.length;i++){
-if (inv[i][0] == kw){
-msg = inv[i][1]
-break;
-}
+for(i = 0; i < inv.length;i++){
+    if (inv[i][0] == kw){
+        msg = inv[i][1]
+        break
+    }
 }
 
 // returns if word is not found in data frame
 if (msg == "Oh no! We can't find that word!"){
-res.send(msg)
-return
+    res.send(msg)
+    return
 }
-
+debug('4')
+// finds the links and their associated titles and descriptions
+// stores them to be outputted
 output = []
 
 for(i = 0; i < msg.length; i++)
-for(j = 0; j < data.length; j++)
-if (msg[i][0] == data[j][1])
-    output.push([data[j][0],data[j][1],data[j][2],msg[i][1]])
+    for(j = 0; j < data.length; j++)
+        if (msg[i][0] == data[j][1])
+            output.push([data[j][0],data[j][1],data[j][2],msg[i][1]])
 
-console.log(output)
+    console.log("index.js out: ")
+    console.log(output)
 
-res.send(JSON.stringify(output))
+    res.send(JSON.stringify(output))
+    //res.send(output)
 })
-
+console.log('5')
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
