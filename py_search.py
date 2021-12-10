754312df54c919476b0e0280c4ca59f26e19e307
Jake Sanghavi <57878447+jakesanghavi@users.noreply.github.com>
2021-12-09 19:54:57 -0500

    Update py_search.py

--- py_search.py
+++ py_search.py
@@ -7,19 +7,32 @@ data.columns = ['word', 'links']
 data2 = pd.read_csv("nspiderdata.csv", delimiter="~")
 data2.columns = ['title', 'link', 'desc']
 
-word = input("What are you looking for?" + "\n").lower()
+flag = True
 
-searched = data.loc[data.word == word]
-searched = searched.dropna()
+while flag:
+    word = input("What are you looking for?" + "\n").lower()
 
-if len(searched) == 0:
-    print("No results")
-else:
-    links = searched.links.astype(str).apply(eval).values[0]
-    if len(links) > 10:
-        links = links[:10]
-    links = [x[0] for x in links]
-    for link in links:
-        print(link)
-        print(data2.loc[data2['link'] == link, 'title'].iloc[0])
-        print(str(data2.loc[data2['link'] == link, 'desc'].iloc[0][:100]) + "\n")
+    searched = data.loc[data.word == word]
+    searched = searched.dropna()
+
+    if len(searched) == 0:
+        print("No results")
+        resp = input("Would you like to search again? (y/n)").lower()
+        if resp == 'y' or resp == 'yes':
+            continue
+        else:
+            flag = False
+    else:
+        links = searched.links.astype(str).apply(eval).values[0]
+        if len(links) > 10:
+            links = links[:10]
+        links = [x[0] for x in links]
+        for link in links:
+            print(link)
+            print(data2.loc[data2['link'] == link, 'title'].iloc[0])
+            print(str(data2.loc[data2['link'] == link, 'desc'].iloc[0][:100]) + "\n")
+        resp = input("Would you like to search again? (y/n)").lower()
+        if resp == 'y' or resp == 'yes':
+            continue
+        else:
+            flag = False
